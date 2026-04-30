d6452945d61ad075ad866754346ba6058842905a
solrac031504 <carlos.gonzalez031504@gmail.com>
2026-04-29 21:16:15 -0400

    Genre hierarchy entity

---
+++ musicapp-server/src/infrastructure/data-access/entities/GenreHierarchy.ts
@@ -0,0 +1,54 @@
+import { 
+    Column,
+    Entity, 
+    PrimaryColumn
+} from "typeorm";
+
+@Entity('genre_hierarchy')
+export class GenreHierarchy {
+    @PrimaryColumn({
+        name: 'genre_id',
+        type: 'int'
+    })
+    genreId!: number;
+
+    @PrimaryColumn({
+        name: 'parent_genre_id',
+        type: 'int'
+    })
+    parentGenreId!: number;
+
+    @Column({ 
+        name: 'created_utc', 
+        type: 'timestamptz', 
+        insert: false,
+        utc: true
+    })
+    createdUtc!: Date;
+
+    @Column({ 
+        name: 'created_by', 
+        type: 'varchar', 
+        length: 255, 
+        insert: false 
+    })
+    createdBy!: string;
+
+    @Column({ 
+        name: 'modified_utc', 
+        type: 'timestamptz', 
+        nullable: true, 
+        insert: false,
+        utc: true
+    })
+    modifiedUtc!: Date | null;
+
+    @Column({ 
+        name: 'modified_by', 
+        type: 'varchar', 
+        length: 255, 
+        nullable: true, 
+        insert: false 
+    })
+    modifiedBy!: string | null;
+}
