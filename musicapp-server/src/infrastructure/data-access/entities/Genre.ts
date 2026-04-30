63954fce5db06e24473365292e9726f3bfd677e2
solrac031504 <carlos.gonzalez031504@gmail.com>
2026-04-29 21:14:09 -0400

    Genre entity

---
+++ musicapp-server/src/infrastructure/data-access/entities/Genre.ts
@@ -0,0 +1,62 @@
+import { 
+    Column, 
+    Entity, 
+    PrimaryGeneratedColumn 
+} from "typeorm";
+
+@Entity('genre')
+export class Genre {
+    @PrimaryGeneratedColumn({
+        name: 'genre_id',
+        type: 'int'
+    })
+    genreId!: number;
+
+    @Column({
+        name: 'genre_name',
+        type: 'varchar',
+        length: 255
+    })
+    genreName!: string;
+
+    @Column({
+        name: 'description',
+        type: 'varchar',
+        length: 4000
+    })
+    description!: string;
+
+    @Column({ 
+        name: 'created_utc', 
+        type: 'timestamptz', 
+        insert: false,
+        utc: true
+    })
+    createdUtc!: Date;
+
+    @Column({ 
+        name: 'created_by', 
+        type: 'varchar', 
+        length: 255, 
+        insert: false 
+    })
+    createdBy!: string;
+
+    @Column({ 
+        name: 'modified_utc', 
+        type: 'timestamptz', 
+        nullable: true, 
+        insert: false,
+        utc: true
+    })
+    modifiedUtc!: Date | null;
+
+    @Column({ 
+        name: 'modified_by', 
+        type: 'varchar', 
+        length: 255, 
+        nullable: true, 
+        insert: false 
+    })
+    modifiedBy!: string | null;
+}
