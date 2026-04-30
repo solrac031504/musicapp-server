86f2f6524fe4835f8e11af05af7063a4c355f3d8
solrac031504 <carlos.gonzalez031504@gmail.com>
2026-04-29 21:26:09 -0400

    producer entity

---
+++ musicapp-server/src/infrastructure/data-access/entities/Producer.ts
@@ -0,0 +1,61 @@
+import { 
+Column,
+    Entity,
+    PrimaryGeneratedColumn
+} from "typeorm";
+
+@Entity('producer')
+export class Producer {
+    @PrimaryGeneratedColumn({
+        name: 'producer_id',
+        type: 'int'
+    })
+    producerId!: number;
+
+    @Column({
+        name: 'producer_name',
+        type: 'varchar',
+        length: 255
+    })
+    producerName!: string;
+
+    @Column({
+        name: 'is_active',
+        type: 'boolean'
+    })
+    isActive!: boolean;
+
+    @Column({ 
+        name: 'created_utc', 
+        type: 'timestamptz', 
+        insert: false,
+        utc: true
+    })
+    createdUtc!: Date;
+
+    @Column({ 
+        name: 'created_by', 
+        type: 'varchar', 
+        length: 255, 
+        insert: false 
+    })
+    createdBy!: string;
+
+    @Column({ 
+        name: 'modified_utc', 
+        type: 'timestamptz', 
+        nullable: true, 
+        insert: false,
+        utc: true
+    })
+    modifiedUtc!: Date | null;
+
+    @Column({ 
+        name: 'modified_by', 
+        type: 'varchar', 
+        length: 255, 
+        nullable: true, 
+        insert: false 
+    })
+    modifiedBy!: string | null;
+}
