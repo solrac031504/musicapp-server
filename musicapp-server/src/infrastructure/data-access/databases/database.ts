0e233cc438f85eccb6e1e29bd3c030f6d1696980
solrac031504 <carlos.gonzalez031504@gmail.com>
2026-04-29 20:35:53 -0400

    Got database connection working

--- musicapp-server/src/infrastructure/data-access/databases/database.ts
+++ musicapp-server/src/infrastructure/data-access/databases/database.ts
@@ -7,17 +7,17 @@ import { DataSource } from 'typeorm';
 
 export const Database = new DataSource({
   type: 'postgres',
-  host: Deno.env.get('DB_HOST') ?? 'localhost',
-  port: parseInt(Deno.env.get('DB_PORT') ?? '5432'),
+  host: Deno.env.get('DB_HOST'),
+  port: parseInt(Deno.env.get('DB_PORT')!),
   username: Deno.env.get('DB_USER'),
   password: Deno.env.get('DB_PASSWORD'),
   database: Deno.env.get('DB_NAME'),
   synchronize: false,
   logging: true,
   entities: [
-    // Add entity classes here explicitly, e.g.:
-    // Song,
+    "../entities/*.ts"
   ],
-  subscribers: [],
-  migrations: [],
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
