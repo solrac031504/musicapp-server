d515c71f9cb61df6fa57a3b6ba53a41433440adb
solrac031504 <carlos.gonzalez031504@gmail.com>
2026-05-02 15:42:30 -0400

    Artist entity mapper

--- musicapp-server/src/infrastructure/common/mappers/entity-mapper.ts
+++ musicapp-server/src/infrastructure/common/mappers/entity-mapper.ts
@@ -1,7 +1,7 @@
 import { BaseEntity } from "../entities/base-entity.ts";
 import { BaseModel } from "../../../domain/common/models/base.model.ts";
 
-export abstract class EntityMapper<T extends typeof BaseModel, K extends typeof BaseEntity> {
+export abstract class EntityMapper<T extends BaseModel, K extends BaseEntity> {
     abstract toModel(entity: K): T;
 
     abstract fromModel(model: T): K;
