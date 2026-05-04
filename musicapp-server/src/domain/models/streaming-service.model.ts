ec510ea2bd2859e67e5b0c3d04118632780c312d
solrac031504 <carlos.gonzalez031504@gmail.com>
2026-05-04 19:59:30 -0400

    Streaming service model and entity mapper

---
+++ musicapp-server/src/domain/models/streaming-service.model.ts
@@ -0,0 +1,5 @@
+import { BaseIdModel } from "../common/models/base-id.model.ts";
+
+export class StreamingServiceModel extends BaseIdModel {
+    streamingServiceName!: string;
+}
---
+++ musicapp-server/src/infrastructure/logic/mappers/streaming-service.entity-mapper.ts
@@ -0,0 +1,19 @@
+import { StreamingServiceModel } from "../../../domain/models/streaming-service.model.ts";
+import { EntityMapper } from "../../common/mappers/entity-mapper.ts";
+import { StreamingServiceEntity } from "../../data-access/entities/streaming-service.entity.ts";
+
+export class StreamingServiceEntityMapper extends EntityMapper<StreamingServiceModel, StreamingServiceEntity> {
+    public override toModel(entity: StreamingServiceEntity): StreamingServiceModel {
+        return {
+            id: entity.streamingServiceId,
+            streamingServiceName: entity.streamingServiceName
+        } as StreamingServiceModel
+    }
+
+    public override fromModel(model: StreamingServiceModel): StreamingServiceEntity {
+        return {
+            streamingServiceId: model.id,
+            streamingServiceName: model.streamingServiceName
+        } as StreamingServiceEntity
+    }
+}
