f2b9b85b72458a793e2ed54bb345408896daf8eb
solrac031504 <carlos.gonzalez031504@gmail.com>
2026-05-04 20:13:05 -0400

    Explicitly adding entities to database

--- musicapp-server/src/infrastructure/data-access/databases/database.ts
+++ musicapp-server/src/infrastructure/data-access/databases/database.ts
@@ -1,4 +1,18 @@
 import { DataSource } from 'typeorm';
+import { ArtistGroupEntity } from "../entities/artist-group.entity.ts";
+import { ArtistGroupMembershipEntity } from "../entities/artist-group-membership.entity.ts";
+import { ArtistEntity } from "../entities/artist.entity.ts";
+import { GenreHierarchyEntity } from "../entities/genre-hierarchy.entity.ts";
+import { GenreEntity } from "../entities/genre.entity.ts";
+import { ProducerGroupMembershipEntity } from "../entities/producer-group-membership.entity.ts";
+import { ProducerGroupEntity } from "../entities/producer-group.entity.ts";
+import { ProducerEntity } from "../entities/producer.entity.ts";
+import { ProjectTypeEntity } from "../entities/project-type.entity.ts";
+import { ProjectEntity } from "../entities/project.entity.ts";
+import { SceneEntity } from "../entities/scene.entity.ts";
+import { SongEntity } from "../entities/song.entity.ts";
+import { StreamingServiceEntity } from "../entities/streaming-service.entity.ts";
+import { UserLoginEntity } from "../entities/user-login.entity.ts";
 
 export const Database = new DataSource({
   type: 'postgres',
@@ -10,7 +24,20 @@ export const Database = new DataSource({
   synchronize: false,
   logging: true,
   entities: [
-    "../entities/*.ts"
+    ArtistEntity,
+    ArtistGroupEntity,
+    ArtistGroupMembershipEntity,
+    GenreEntity,
+    GenreHierarchyEntity,
+    ProducerEntity,
+    ProducerGroupEntity,
+    ProducerGroupMembershipEntity,
+    ProjectTypeEntity,
+    ProjectEntity,
+    SceneEntity,
+    SongEntity,
+    StreamingServiceEntity,
+    UserLoginEntity
   ],
   ssl: {
     rejectUnauthorized: false
