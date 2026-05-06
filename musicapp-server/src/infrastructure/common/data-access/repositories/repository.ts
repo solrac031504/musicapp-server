dfe1a700e14753c659ea605df43b3dec21aee580
solrac031504 <carlos.gonzalez031504@gmail.com>
2026-05-05 21:31:36 -0400

    Cleaning up and finishing repo

--- musicapp-server/src/infrastructure/common/data-access/repositories/repository.ts
+++ musicapp-server/src/infrastructure/common/data-access/repositories/repository.ts
@@ -7,38 +7,38 @@ import { BaseModel } from "../../../../domain/common/models/base.model.ts";
 import { BaseEntity } from "../entities/base-entity.ts";
 import { BaseRepository } from "../../../../domain/common/repositories/base-repository.ts";
 
+// TODO: Maybe cleanup use of save()? insert() and update() exist, but don't return the result on operation...
+
 export abstract class Repository<T extends BaseModel, K extends BaseEntity, Y extends DataSource> extends BaseRepository<T> {
-    private readonly dataSource: Y;
     protected readonly repo: TypeOrmRepository<K>;
 
     constructor(dataSource: Y, entity: EntityTarget<K>) {
         super();
-        this.dataSource = dataSource;
         this.repo = dataSource.getRepository<K>(entity);
     }
 
     public override async add(model: T): Promise<T> {
         const entity = this.fromModel(model);
 
-        await this.repo.insert(entity);
+        const result = await this.repo.save(entity);
 
-        return this.toModel(entity);
+        return this.toModel(result);
     }
 
     public override async addMany(models: T[]): Promise<T[]> {
         const entities = models.map(m => this.fromModel(m));
 
-        await this.repo.insert(entities);
+        const results = await this.repo.save(entities);
 
-        return entities.map(e => this.toModel(e));
+        return results.map(e => this.toModel(e));
     }
 
     public override async update(model: T): Promise<T> {
         const entity = this.fromModel(model);
 
-        await this.repo.save(entity);
+        const result = await this.repo.save(entity);
 
-        return this.toModel(entity);
+        return this.toModel(result);
     }
 
     public override async delete(id: number): Promise<void> {
@@ -46,7 +46,7 @@ export abstract class Repository<T extends BaseModel, K extends BaseEntity, Y ex
 
         if (!entity) return;
 
-        this.repo.remove(entity!);
+        await this.repo.remove(entity);
     }
 
     public override async list(): Promise<T[]> {
@@ -58,12 +58,12 @@ export abstract class Repository<T extends BaseModel, K extends BaseEntity, Y ex
     public override async getById(id: number): Promise<T | null> {
         const entity = await this.findOneById(id);
 
-        return entity ? this.toModel(entity) : null;
+        if (!entity) return null;
+
+        return this.toModel(entity);
     }
 
     public abstract findOneById(id: number): Promise<K | null>;
-
-    public abstract findOneByKey(model: T): Promise<K | null>;
 
     public abstract toModel(entity: K): T;
 
