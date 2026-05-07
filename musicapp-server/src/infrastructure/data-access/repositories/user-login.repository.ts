ca71be6495ae92a09f01b28ebafc91520e43dfdc
solrac031504 <carlos.gonzalez031504@gmail.com>
2026-05-06 21:39:05 -0400

    User login repository. Maybe remove later

---
+++ musicapp-server/src/infrastructure/data-access/repositories/user-login.repository.ts
@@ -0,0 +1,15 @@
+import { DataSource } from "typeorm";
+import { UserLoginModel } from "../../../domain/models/user-login.model.ts";
+import { Repository } from "../../common/data-access/repositories/repository.ts";
+import { UserLoginEntityMapper } from "../../logic/mappers/user-login.entity-mapper.ts";
+import { UserLoginEntity } from "../entities/user-login.entity.ts";
+
+export class UserLoginRepository extends Repository<UserLoginModel, UserLoginEntity, UserLoginEntityMapper, DataSource> {
+    constructor(dataSource: DataSource) {
+        super(dataSource, UserLoginEntity, new UserLoginEntityMapper);
+    }
+
+    protected override async findOneById(id: number): Promise<UserLoginEntity | null> {
+        return await this.repo.findOneBy({ userLoginId: id });
+    }
+}
