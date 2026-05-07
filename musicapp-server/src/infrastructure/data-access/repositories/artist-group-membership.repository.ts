8a0c2a011aba82d68a2c61917c6e7ab5f5a71b16
solrac031504 <carlos.gonzalez031504@gmail.com>
2026-05-06 21:19:43 -0400

    Artist group membership repository

---
+++ musicapp-server/src/infrastructure/data-access/repositories/artist-group-membership.repository.ts
@@ -0,0 +1,16 @@
+import { DataSource } from "typeorm";
+import { ArtistGroupMembershipModel } from "../../../domain/models/artist-group-membership.model.ts";
+import { Repository } from "../../common/data-access/repositories/repository.ts";
+import { ArtistGroupMembershipEntityMapper } from "../../logic/mappers/artist-group-membership.entity-mapper.ts";
+import { ArtistGroupMembershipEntity } from "../entities/artist-group-membership.entity.ts";
+import { ArtistEntity } from "../entities/artist.entity.ts";
+
+export class ArtistGroupMembershipRepository extends Repository<ArtistGroupMembershipModel, ArtistGroupMembershipEntity, ArtistGroupMembershipEntityMapper, DataSource> {
+    constructor(dataSource: DataSource) {
+        super(dataSource, ArtistEntity, new ArtistGroupMembershipEntityMapper);
+    }
+
+    protected override async findOneById(id: number): Promise<ArtistGroupMembershipEntity | null> {
+        return await this.repo.findOneBy({ artistGroupMembershipId: id });
+    }
+}
