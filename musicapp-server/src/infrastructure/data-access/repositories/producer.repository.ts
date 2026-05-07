1e5ce2ca70e388d75bd1771ec90f0e97e7637caa
solrac031504 <carlos.gonzalez031504@gmail.com>
2026-05-06 21:29:06 -0400

    Producer repository

---
+++ musicapp-server/src/infrastructure/data-access/repositories/producer.repository.ts
@@ -0,0 +1,15 @@
+import { DataSource } from "typeorm";
+import { ProducerModel } from "../../../domain/models/producer.model.ts";
+import { Repository } from "../../common/data-access/repositories/repository.ts";
+import { ProducerEntityMapper } from "../../logic/mappers/producer.entity-mapper.ts";
+import { ProducerEntity } from "../entities/producer.entity.ts";
+
+export class ProducerRepository extends Repository<ProducerModel, ProducerEntity, ProducerEntityMapper, DataSource> {
+    constructor(dataSource: DataSource) {
+        super(dataSource, ProducerEntity, new ProducerEntityMapper);
+    }
+
+    protected override async findOneById(id: number): Promise<ProducerEntity | null> {
+        return await this.repo.findOneBy({ producerId: id });
+    }
+}
