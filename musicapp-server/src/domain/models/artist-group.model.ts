5565ffb45f9c86562b781c80b4b664a5564df819
solrac031504 <carlos.gonzalez031504@gmail.com>
2026-05-22 20:50:52 -0400

    Adding settable created and modified by for artist group

--- musicapp-server/src/domain/models/artist-group.model.ts
+++ musicapp-server/src/domain/models/artist-group.model.ts
@@ -1,7 +1,11 @@
 import { BaseIdModel } from "../common/models/base-id.model.ts";
 
 export class ArtistGroupModel extends BaseIdModel {
-    artistGroupName!: string;
+	artistGroupName!: string;
 
-    isActive!: boolean;
+	isActive!: boolean;
+
+	createdBy: string = null!;
+
+	modifiedBy: string | null = null;
 }
--- musicapp-server/src/infrastructure/logic/mappers/artist-group.entity-mapper.ts
+++ musicapp-server/src/infrastructure/logic/mappers/artist-group.entity-mapper.ts
@@ -2,21 +2,24 @@ import { ArtistGroupModel } from "../../../domain/models/artist-group.model.ts";
 import { EntityMapper } from "../../common/mappers/entity-mapper.ts";
 import { ArtistGroupEntity } from "../../data-access/entities/artist-group.entity.ts";
 
-export class ArtistGroupEntityMapper
-    extends EntityMapper<ArtistGroupModel, ArtistGroupEntity> {
-    public override toModel(entity: ArtistGroupEntity): ArtistGroupModel {
-        return {
-            id: entity.artistGroupId,
-            artistGroupName: entity.artistGroupName,
-            isActive: entity.isActive,
-        } as ArtistGroupModel;
-    }
+export class ArtistGroupEntityMapper extends EntityMapper<ArtistGroupModel, ArtistGroupEntity> {
+	public override toModel(entity: ArtistGroupEntity): ArtistGroupModel {
+		return {
+			id: entity.artistGroupId,
+			artistGroupName: entity.artistGroupName,
+			isActive: entity.isActive,
+			createdBy: entity.createdBy,
+			modifiedBy: entity.modifiedBy,
+		} as ArtistGroupModel;
+	}
 
-    public override fromModel(model: ArtistGroupModel): ArtistGroupEntity {
-        return {
-            artistGroupId: model.id,
-            artistGroupName: model.artistGroupName,
-            isActive: model.isActive,
-        } as ArtistGroupEntity;
-    }
+	public override fromModel(model: ArtistGroupModel): ArtistGroupEntity {
+		return {
+			artistGroupId: model.id,
+			artistGroupName: model.artistGroupName,
+			isActive: model.isActive,
+			createdBy: model.createdBy,
+			modifiedBy: model.modifiedBy,
+		} as ArtistGroupEntity;
+	}
 }
