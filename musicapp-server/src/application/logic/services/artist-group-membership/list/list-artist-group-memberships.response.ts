06daf2650e29703c55267893bb1e948e6ca80d5d
solrac031504 <carlos.gonzalez031504@gmail.com>
2026-05-23 17:29:50 -0400

    Correcting DTO module import

--- musicapp-server/src/application/logic/services/artist-group-membership/list/list-artist-group-memberships.response.ts
+++ musicapp-server/src/application/logic/services/artist-group-membership/list/list-artist-group-memberships.response.ts
@@ -1,5 +1,5 @@
 import { BaseListResponse } from "../../../../common/responses/base-list-response.ts";
-import { ArtistGroupMembershipDTO } from "../../../dto/artist-group-membership.dto.ts";
+import { ArtistGroupMembershipDTO } from "../../../../dto/artist-group-membership.dto.ts";
 
 export class ListArtistGroupMembershipsResponse extends BaseListResponse<ArtistGroupMembershipDTO> {
 	constructor(items: ArtistGroupMembershipDTO[] = []) {
--- musicapp-server/src/application/logic/services/artist-group-membership/update/update-artist-group-membership.response.ts
+++ musicapp-server/src/application/logic/services/artist-group-membership/update/update-artist-group-membership.response.ts
@@ -1,5 +1,5 @@
 import { BaseItemResponse } from "../../../../common/responses/base-item-response.ts";
-import { ArtistGroupMembershipDTO } from "../../../dto/artist-group-membership.dto.ts";
+import { ArtistGroupMembershipDTO } from "../../../../dto/artist-group-membership.dto.ts";
 
 export class UpdateArtistGroupMembershipResponse extends BaseItemResponse<ArtistGroupMembershipDTO> {
 	constructor(item: ArtistGroupMembershipDTO | null = null) {
