68e5961088b03a4d5f8bc41f0710d3f58c5914e6
solrac031504 <carlos.gonzalez031504@gmail.com>
2026-05-23 23:49:38 -0400

    Linting

--- musicapp-server/src/application/logic/mappers/request-mappers/add-project-type.request-mapper.ts
+++ musicapp-server/src/application/logic/mappers/request-mappers/add-project-type.request-mapper.ts
@@ -3,12 +3,12 @@ import { BaseRequestMapper } from "../../../common/mappers/base-request-mapper.t
 import { AddProjectTypeRequest } from "../../services/project-type/add/add-project-type.request.ts";
 
 export class AddProjectTypeRequestMapper extends BaseRequestMapper<AddProjectTypeRequest, ProjectTypeModel> {
-    public override map(req: AddProjectTypeRequest): ProjectTypeModel {
-        return {
-            id: req.item.id,
-            projectTypeName: req.item.projectTypeName,
-            createdBy: req.item.createdBy,
-            modifiedBy: req.item.modifiedBy,
-        } as ProjectTypeModel;
-    }
+	public override map(req: AddProjectTypeRequest): ProjectTypeModel {
+		return {
+			id: req.item.id,
+			projectTypeName: req.item.projectTypeName,
+			createdBy: req.item.createdBy,
+			modifiedBy: req.item.modifiedBy,
+		} as ProjectTypeModel;
+	}
 }
--- musicapp-server/src/application/logic/mappers/request-mappers/update-project-type.request-mapper.ts
+++ musicapp-server/src/application/logic/mappers/request-mappers/update-project-type.request-mapper.ts
@@ -3,12 +3,12 @@ import { BaseRequestMapper } from "../../../common/mappers/base-request-mapper.t
 import { UpdateProjectTypeRequest } from "../../services/project-type/update/update-project-type.request.ts";
 
 export class UpdateProjectTypeRequestMapper extends BaseRequestMapper<UpdateProjectTypeRequest, ProjectTypeModel> {
-    public override map(req: UpdateProjectTypeRequest): ProjectTypeModel {
-        return {
-            id: req.item.id,
-            projectTypeName: req.item.projectTypeName,
-            createdBy: req.item.createdBy,
-            modifiedBy: req.item.modifiedBy,
-        } as ProjectTypeModel;
-    }
+	public override map(req: UpdateProjectTypeRequest): ProjectTypeModel {
+		return {
+			id: req.item.id,
+			projectTypeName: req.item.projectTypeName,
+			createdBy: req.item.createdBy,
+			modifiedBy: req.item.modifiedBy,
+		} as ProjectTypeModel;
+	}
 }
