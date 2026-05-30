901e4703427e745b91107f22ec7ed3bf42005158
solrac031504 <carlos.gonzalez031504@gmail.com>
2026-05-29 23:32:03 -0400

    Fixing decorators and cleaning up

--- musicapp-server/src/application/logic/mappers/response-mappers/list-genre-hierarchy-by-genre.request-mapper.ts
+++ musicapp-server/src/application/logic/mappers/response-mappers/list-genre-hierarchy-by-genre.request-mapper.ts
@@ -1,10 +1,10 @@
 import { BaseListResponseMapper } from "@application/common/mappers/base-list-response-mapper.ts";
 import { GenreHierarchyViewDTO } from "@application/dto/genre-hierarchy-view.dto.ts";
-import { ListGenreHierarchyViewByGenreResponse } from "@application/logic/services/genre-hierarchy-view/list/by-genre/list-genre-hierarchy-view-by-genre.response.ts";
+import { ListGenreHierarchyByGenreResponse } from "@application/logic/services/genre-hierarchy/list/by-genre/list-genre-hierarchy-by-genre.response.ts";
 import { GenreHierarchyViewModel } from "@domain/models/genre-hierarchy-view.model.ts";
 
-export class ListGenreHierarchyByGenreResponseMapper extends BaseListResponseMapper<ListGenreHierarchyViewByGenreResponse, GenreHierarchyViewModel> {
-	public override map(models: GenreHierarchyViewModel[]): ListGenreHierarchyViewByGenreResponse {
-		return new ListGenreHierarchyViewByGenreResponse(models.map((m) => new GenreHierarchyViewDTO(m)));
+export class ListGenreHierarchyByGenreResponseMapper extends BaseListResponseMapper<ListGenreHierarchyByGenreResponse, GenreHierarchyViewModel> {
+	public override map(models: GenreHierarchyViewModel[]): ListGenreHierarchyByGenreResponse {
+		return new ListGenreHierarchyByGenreResponse(models.map((m) => new GenreHierarchyViewDTO(m)));
 	}
 }
--- musicapp-server/src/application/logic/services/genre-hierarchy/list/by-genre/list-genre-hierarchy-by-genre.service.ts
+++ musicapp-server/src/application/logic/services/genre-hierarchy/list/by-genre/list-genre-hierarchy-by-genre.service.ts
@@ -1,10 +1,10 @@
 import { BaseService } from "@application/common/services/base-service.ts";
 import { ListGenreHierarchyByGenreResponseMapper } from "@application/logic/mappers/response-mappers/list-genre-hierarchy-by-genre.request-mapper.ts";
-import { ListGenreHierarchyViewByGenreRequest } from "@application/logic/services/genre-hierarchy-view/list/by-genre/list-genre-hierarchy-view-by-genre.request.ts";
-import { ListGenreHierarchyViewByGenreResponse } from "@application/logic/services/genre-hierarchy-view/list/by-genre/list-genre-hierarchy-view-by-genre.response.ts";
+import { ListGenreHierarchyByGenreRequest } from "@application/logic/services/genre-hierarchy/list/by-genre/list-genre-hierarchy-by-genre.request.ts";
+import { ListGenreHierarchyByGenreResponse } from "@application/logic/services/genre-hierarchy/list/by-genre/list-genre-hierarchy-by-genre.response.ts";
 import { GenreHierarchyViewRepository } from "@infrastructure/data-access/repositories/genre-hierarchy-view.repository.ts";
 
-export class ListGenreHierarchyByGenreService extends BaseService<ListGenreHierarchyViewByGenreRequest, ListGenreHierarchyViewByGenreResponse> {
+export class ListGenreHierarchyByGenreService extends BaseService<ListGenreHierarchyByGenreRequest, ListGenreHierarchyByGenreResponse> {
 	private readonly repo;
 	private readonly resMapper = new ListGenreHierarchyByGenreResponseMapper();
 
@@ -13,7 +13,7 @@ export class ListGenreHierarchyByGenreService extends BaseService<ListGenreHiera
 		this.repo = repo;
 	}
 
-	public override async execute(req: ListGenreHierarchyViewByGenreRequest): Promise<ListGenreHierarchyViewByGenreResponse> {
+	public override async execute(req: ListGenreHierarchyByGenreRequest): Promise<ListGenreHierarchyByGenreResponse> {
 		const models = await this.repo.listByGenreId(req.id);
 
 		return this.resMapper.map(models);
