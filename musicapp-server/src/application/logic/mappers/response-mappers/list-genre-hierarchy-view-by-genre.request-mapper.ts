58a7617fea82c922be61c806116dcc9916faf20e
solrac031504 <carlos.gonzalez031504@gmail.com>
2026-05-29 21:26:21 -0400

    Using path aliases

--- musicapp-server/src/application/logic/mappers/response-mappers/list-genre-hierarchy-view-by-genre.request-mapper.ts
+++ musicapp-server/src/application/logic/mappers/response-mappers/list-genre-hierarchy-view-by-genre.request-mapper.ts
@@ -1,7 +1,7 @@
-import { GenreHierarchyViewModel } from "../../../../domain/models/genre-hierarchy-view.model.ts";
-import { BaseListResponseMapper } from "../../../common/mappers/base-list-response-mapper.ts";
-import { GenreHierarchyViewDTO } from "../../../dto/genre-hierarchy-view.dto.ts";
-import { ListGenreHierarchyViewByGenreResponse } from "../../services/genre-hierarchy-view/list/by-genre/list-genre-hierarchy-view-by-genre.response.ts";
+import { BaseListResponseMapper } from "@application/common/mappers/base-list-response-mapper.ts";
+import { GenreHierarchyViewDTO } from "@application/dto/genre-hierarchy-view.dto.ts";
+import { ListGenreHierarchyViewByGenreResponse } from "@application/logic/services/genre-hierarchy-view/list/by-genre/list-genre-hierarchy-view-by-genre.response.ts";
+import { GenreHierarchyViewModel } from "@domain/models/genre-hierarchy-view.model.ts";
 
 export class ListGenreHierarchyViewByGenreResponseMapper extends BaseListResponseMapper<ListGenreHierarchyViewByGenreResponse, GenreHierarchyViewModel> {
 	public override map(models: GenreHierarchyViewModel[]): ListGenreHierarchyViewByGenreResponse {
